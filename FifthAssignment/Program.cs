566d392165b0cadddbf8cff93972447c74cb4d6f
Bassam Ibrahim Abu El-Saud <bassamb672005@gmail.com>
2026-09-09 18:51:56 +0300

    first question

--- FifthAssignment/Program.cs
+++ FifthAssignment/Program.cs
@@ -1,7 +1,7 @@
 ﻿
 class program {
     public static void main(String[] args) {
-        #region first qurstion
+        #region first question
         /*
          a) DeliveryAddress is a struct so it is a value type  so when we copy it creates an indpendent copy  modifing the copy does not affect the original struct.
          b) DeliveryAddress is a class so it is a reference type so when we copy it creates a reference to the same object modifing the copy affects the original class.
