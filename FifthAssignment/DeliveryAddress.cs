ae9ead617c19e4dc14348ef0cb952b4d868ea65e
Bassam Ibrahim Abu El-Saud <bassamb672005@gmail.com>
2026-09-09 19:03:29 +0300

    DeliveryAddress struct

---
+++ FifthAssignment/DeliveryAddress.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FifthAssignment
+{
+    #region DeliveryAddress struct
+    public struct DeliveryAddress
+    {
+        public string city;
+        public string street;
+        public int BuildingNumber;
+        public DeliveryAddress(string city, string street, int buildingNumber)
+        {
+            this.city = city;
+            this.street = street;
+            this.BuildingNumber = buildingNumber;
+        }
+        public string GetFullAddress()
+        {
+            return $"{city}, {street}, {BuildingNumber}";
+
+        }
+    }
+}
+#endregion
