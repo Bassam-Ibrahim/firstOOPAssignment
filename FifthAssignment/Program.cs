5bfd48193b7a2e79742af13336a2b0daf4bd9a58
Bassam Ibrahim Abu El-Saud <bassamb672005@gmail.com>
2026-09-09 18:51:34 +0300

    Add project files.

---
+++ FifthAssignment/Program.cs
@@ -0,0 +1,17 @@
+﻿
+class program {
+    public static void main(String[] args) {
+        #region first qurstion
+        /*
+         a) DeliveryAddress is a struct so it is a value type  so when we copy it creates an indpendent copy  modifing the copy does not affect the original struct.
+         b) DeliveryAddress is a class so it is a reference type so when we copy it creates a reference to the same object modifing the copy affects the original class.
+         */
+        #endregion
+
+
+
+
+    }
+
+
+}
