f7f32e51ceef911125dfabd94b5edf5c994340eb
Bassam Ibrahim Abu El-Saud <bassamb672005@gmail.com>
2026-09-09 18:57:38 +0300

    Second question

--- FifthAssignment/Program.cs
+++ FifthAssignment/Program.cs
@@ -7,6 +7,17 @@ class program {
          b) DeliveryAddress is a class so it is a reference type so when we copy it creates a reference to the same object modifing the copy affects the original class.
          */
         #endregion
+        #region Second question
+        /*
+         a) 
+             1. Fields Are Public so  modifing it Dirctly
+             2. No Validations on Data so we can set any value to it
+             3. NO Encapsulation so we can access it from anywhere
+         b) 
+            private fields hide the internal data 
+            so the properties provide ways to access and modifing this field and Manage the conditions of the data and also we can add validation to the data before setting it to the field.
+          */
+        #endregion
 
 
 
